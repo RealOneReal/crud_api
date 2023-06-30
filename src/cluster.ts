b5b10f0d4fe215da4be2468e7eb650efd12a3404
Aliaksandr Yarokhin <aliaksandr_yarokhin@epam.com>
2023-06-30 23:53:15 +0300

    add: balancer

--- src/cluster.ts
+++ src/cluster.ts
@@ -1,24 +1,59 @@
 import cluster from 'node:cluster';
 import { availableParallelism } from 'node:os';
 import http from 'node:http';
+import { URL } from 'node:url';
 import { routes } from './controller/routes';
+import 'dotenv/config';
 
-const numCPUs = availableParallelism();
+const numCPUS = availableParallelism();
+const START_PORT = Number(process.env.PORT) || 4000;
+
+let reqIteration = 0;
 
 if (cluster.isPrimary) {
-  console.log(`Master process ${process.pid} started`);
 
-  for (let i = 0; i < numCPUs; i++) {
-    const worker = cluster.fork({ PORT: 4000 + i});
-    console.log(`Worker ${worker.process.pid} started, listening on port ${4000 + i}`);
+  for (let cpuIndex = 0; cpuIndex < numCPUS; cpuIndex += 1) {
+    cluster.fork();
   }
 
-  cluster.on('exit', (worker, code, signal) => {
-    console.log(`Worker ${worker.process.pid} died`);
+  cluster.on('exit', () => {
+      console.log('Worker died');
+      cluster.fork();
   });
+
+    ((startPort: number) => {
+      http.createServer((req, res) => {
+          const nextRoundRobinPort = getPort(startPort);
+          console.log(`Sending request to port ${nextRoundRobinPort}`);
+          const url = new URL(req.url, `http://${req.headers.host}`);
+          const pathname = url.pathname;
+          const options = {
+              ...url,
+              pathname,
+              port: nextRoundRobinPort,
+              headers: req.headers,
+              method: req.method,
+          };
+
+          req.pipe(
+              http.request(options, (response) => {
+                console.log(`Received response from port ${nextRoundRobinPort}`);
+                  res.writeHead(response.statusCode, response.headers);
+                  response.pipe(res);
+              }),
+          );
+      }).listen(startPort, () => console.log(`Balancer created on PORT ${startPort} `));
+      })(START_PORT);
+
 } else {
+  const workerPort = START_PORT + cluster.worker.id;
   const server = http.createServer(routes);
-  server.listen(process.env.PORT, () => {
-    console.log(`Worker ${process.pid} started, listening on port ${process.env.PORT}`);
-});
+  server.listen(workerPort, () => {
+    console.log(`Balancer listen on port ${workerPort}`)
+  });
 }
+
+const getPort = (startPort: number): number => {
+    reqIteration = reqIteration === numCPUS ? 1 : reqIteration + 1;
+    return reqIteration + startPort;
+};
